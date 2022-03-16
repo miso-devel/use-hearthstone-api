a13a72472d714539ef4eb1d50d1a32f2b380a7ad
nisi0929 <kengo279@icloud.com>
2022-03-17 03:09:00 +0900

    search-cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,17 @@ import { Cards } from "./components/Cards";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-center">app</h1>
+    <div className="App bg-indigo-600">
+      <div className="header w-full bg-indigo-800 shadow-2xl text-white">
+        <h1 className=" py-10 text-center text-4xl">
+          HEARTH STONE CARD SEARCHER
+        </h1>
+      </div>
+      <div className="my-20 text-white">
+        <Cards />
+      </div>
+
       <Back />
-      <Cards />
     </div>
   );
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,28 +1,69 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import jsonCard from "../json-data/cards.json";
-const APIKEY = process.env.React_APP_CARD_API_KEY;
+const APIKEY = process.env.React_APP_SEARCH_CARD_API_KEY;
 export const Cards = () => {
   const [cards, setCards] = useState([]);
+  const [attack, setAttack] = useState(1);
+  const [health, setHealth] = useState(2);
+  const [types, setTypes] = useState("minion");
 
   useEffect(() => {
     axios
       .get(
-        `https://us.api.blizzard.com/hearthstone/cards?locale=ja_JP&manaCost=3&collectible=1&gameMode=constructed&order (deprecated)=desc&access_token=${APIKEY}`
+        `https://us.api.blizzard.com/hearthstone/cards?locale=ja_JP&attack=${attack}&health=${health}&collectible=1&type=${types}&gameMode=constructed&page=1&pageSize=100&sort=name%3Aasc&order (deprecated)=asc&access_token=${APIKEY}`
       )
       .then((res) => {
         setCards(res.data.cards);
       });
-  }, []);
-  console.log(cards);
+  }, [attack, health, types]);
+  console.log(`攻撃力：${attack}  体力：${health}  種類：${types}`);
+
   return (
-    <div className="m-10">
-      <h1 className="text-center font-bold">CARD</h1>
-      <div className="res_card grid grid-cols-4">
+    <div className="grid grid-cols-7">
+      <div className="text-center shadow-xl text-xl col-span-2">
+        <div className="sticky top-0">
+          <p className="py-10 text-2xl">検索欄</p>
+          <div className="p-3">
+            攻撃力：
+            <input
+              className=" rounded-md text-center text-black"
+              value={attack}
+              type="number"
+              onChange={(e) => setAttack(e.target.value)}
+              placeholder="攻撃力"
+              required
+            />
+          </div>
+          <div className="p-3">
+            体力：
+            <input
+              className=" rounded-md text-center text-black"
+              value={health}
+              type="number"
+              onChange={(e) => setHealth(e.target.value)}
+              placeholder="体力"
+              required
+            />
+          </div>
+          <div className="p-3">
+            種類：
+            <input
+              className=" rounded-md text-center text-black"
+              value={types}
+              type="text"
+              onChange={(e) => setTypes(e.target.value)}
+              placeholder="種類"
+              required
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="res_card grid grid-cols-6 m-5 col-span-5">
         {cards.map((c) => {
           return (
-            <div className="grid grid-cols-1 p-5">
-              <p className="py-5 text-center">{c.id}</p>
+            <div className="grid grid-cols-1 p-5" key={c.id}>
+              <p className="py-5 text-center">{c.name}</p>
               <img src={c.image} alt="" />
               <p className="py-3"></p>
             </div>
