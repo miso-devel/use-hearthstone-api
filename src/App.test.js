8deeb665c4d0d97d0aebecb2d91307f1cdc94c72
nisi0929 <kengo279@icloud.com>
2022-04-01 21:53:31 +0900

    delete images

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("ヘッダーがrenderされてるか", () => {
-  render(<App />);
-  const linkElement = screen.getByText("HEARTH STONE CARD SEARCHER");
-  expect(linkElement).toBeInTheDocument();
-});
